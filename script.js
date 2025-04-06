0a47a1d17b76463685c7c15a7153903f5ccdb1f1
Jorge Barajas <jofbm.jb@gmail.com>
2025-04-05 18:25:57 -0600

    modificacion de la seccion de contacto, solo muestra email

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-document.getElementById("contact-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const message = document.getElementById("message").value;
-
-    if (name && email && message) {
-        alert("Thank you, " + name + "! Your message has been sent successfully.");
-        document.getElementById("contact-form").reset();
-    } else {
-        alert("Please fill in all fields.");
-    }
-});
-
 // Function to toggle language based on user or browser preference
 function toggleLanguage(language) {
     const elements = document.querySelectorAll("[data-en]");
